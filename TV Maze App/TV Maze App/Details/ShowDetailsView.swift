6179bcfd37a0ddb3790a61d45fc66efbff3410bd
Leonardo Figueiredo <leocassio.af@gmail.com>
2024-06-08 20:31:22 -0300

    feat: creating ShowDetailsView

---
+++ TV Maze App/TV Maze App/Details/ShowDetailsView.swift
@@ -0,0 +1,191 @@
+//
+//  ShowDetailsView.swift
+//  TV Maze App
+//
+//  Created by Leonardo de Cassio Andrade Figueiredo on 08/06/24.
+//
+
+import UIKit
+import SnapKit
+
+internal class ShowDetailsView: UIView {
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = false
+        return scrollView
+    }()
+    
+    private let contentView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .fill
+        return stackView
+    }()
+    
+    internal let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    internal let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 28, weight: .bold)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    internal let detailsLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+    
+    internal let aliasesStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        return stackView
+    }()
+    
+    internal let aliasesLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.textColor = .white
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    internal let ratingStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        return stackView
+    }()
+    
+    internal let ratingLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.textColor = .white
+        return label
+    }()
+    
+    internal let starImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "star.fill")
+        imageView.tintColor = .yellow
+        return imageView
+    }()
+    
+    internal let summaryTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Summary"
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textColor = .white
+        return label
+    }()
+    
+    internal let summaryLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .white
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    internal override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints()
+    }
+    
+    @available(*, unavailable)
+    required internal init?(coder _: NSCoder) {
+        return nil
+    }
+    
+    private func setupViews() {
+        addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(stackView)
+        
+        stackView.addArrangedSubview(posterImageView)
+        stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(detailsLabel)
+        stackView.addArrangedSubview(aliasesStackView)
+        stackView.addArrangedSubview(ratingStackView)
+        stackView.addArrangedSubview(summaryTitleLabel)
+        stackView.addArrangedSubview(summaryLabel)
+        
+        aliasesStackView.addArrangedSubview(aliasesLabel)
+        
+        ratingStackView.addArrangedSubview(starImageView)
+        ratingStackView.addArrangedSubview(ratingLabel)
+    }
+    
+    private func setupConstraints() {
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.top.equalToSuperview().inset(-60)
+            make.width.equalTo(scrollView)
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.edges.equalTo(contentView)
+        }
+        
+        starImageView.snp.makeConstraints { make in
+            make.size.equalTo(24)
+        }
+    }
+    
+    internal func configureImage(with image: UIImage) {
+        posterImageView.image = image
+        let aspectRatio = image.size.height / image.size.width
+        let screenWidth = UIScreen.main.bounds.width
+        let imageHeight = screenWidth * aspectRatio
+        
+        posterImageView.snp.remakeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalTo(imageHeight)
+        }
+    }
+    
+    internal func configureSummary(with htmlText: String) {
+        if let data = htmlText.data(using: .utf8) {
+            let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+                .documentType: NSAttributedString.DocumentType.html,
+                .characterEncoding: String.Encoding.utf8.rawValue
+            ]
+            if let attributedString = try? NSMutableAttributedString(data: data, options: options, documentAttributes: nil) {
+                let fullRange = NSRange(location: 0, length: attributedString.length)
+                attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 14, weight: .regular), range: fullRange)
+                attributedString.addAttribute(.foregroundColor, value: UIColor.white, range: fullRange)
+                summaryLabel.attributedText = attributedString
+            }
+        }
+    }
+    
+    internal func configureAliases(with aliases: [AliaseModel]) {
+        guard !aliases.isEmpty else { return }
+        let aliasesText = aliases.map { $0.name }.joined(separator: ", ")
+        aliasesLabel.text = "Also known as: \(aliasesText)"
+    }
+
+}
+
---
+++ TV Maze App/TV Maze App/Details/ShowDetailsViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ShowDetailsViewController.swift
+//  TV Maze App
+//
+//  Created by Leonardo de Cassio Andrade Figueiredo on 08/06/24.
+//
+
+import UIKit
+
+internal class ShowDetailsViewController: UIViewController {
+    private let customView = ShowDetailsView()
+    private let presenter: ShowDetailsPresenterInputProtocol
+    
+    internal init(presenter: ShowDetailsPresenterInputProtocol) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    internal required init?(coder _: NSCoder) {
+        return nil
+    }
+    
+    internal override func loadView() {
+        self.view = customView
+        self.view.backgroundColor = .black
+    }
+    
+    internal override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        setupBackButton()
+    }
+    
+    private func setupBackButton() {
+        let backButton = UIButton(type: .custom)
+        backButton.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        backButton.tintColor = .white
+        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        customView.addSubview(backButton)
+        backButton.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(10)
+            make.leading.equalToSuperview().offset(10)
+            make.width.height.equalTo(48)
+        }
+    }
+    
+    @objc private func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+}
+
+extension ShowDetailsViewController: ShowDetailsPresenterOutputProtocol {
+    internal func displayShowDetails(viewModel: ShowDetailsViewModel) {
+        DispatchQueue.main.async {
+            self.customView.titleLabel.text = viewModel.title
+            self.customView.ratingLabel.text = viewModel.rating
+            
+            self.customView.configureSummary(with: viewModel.summary)
+            
+            if let imageUrl = viewModel.posterURL, let url = URL(string: imageUrl) {
+                DispatchQueue.global().async {
+                    if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
+                        DispatchQueue.main.async {
+                            self.customView.configureImage(with: image)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    internal func dispayAliases(aliases: [AliaseModel]) {
+        DispatchQueue.main.async {
+            self.customView.configureAliases(with: aliases)
+        }
+    }
+}
