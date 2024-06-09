8c5e684a3f1eaa207efae68992a15af7bb30f363
Leonardo Figueiredo <leocassio.af@gmail.com>
2024-06-09 13:01:52 -0300

    test: tests for home view controller

---
+++ TV Maze App/TV Maze AppTests/Home/HomePresenterTestCase.swift
@@ -0,0 +1,80 @@
+//
+//  HomePresenterTestCase.swift
+//  TV Maze AppTests
+//
+//  Created by Leonardo de Cassio Andrade Figueiredo on 09/06/24.
+//
+
+import XCTest
+@testable import TV_Maze_App
+
+final class HomePresenterTests: XCTestCase {
+    
+    var sut: HomePresenter!
+    var viewSpy: HomeViewControllerSpy!
+    var interactorSpy: HomeInteractorSpy!
+    var coordinatorSpy: HomeCoordinatorSpy!
+    
+    override func setUp() {
+        super.setUp()
+        viewSpy = HomeViewControllerSpy()
+        interactorSpy = HomeInteractorSpy()
+        coordinatorSpy = HomeCoordinatorSpy()
+        sut = HomePresenter()
+        sut.view = viewSpy
+        sut.interactor = interactorSpy
+        sut.coordinator = coordinatorSpy
+    }
+    
+    override func tearDown() {
+        sut = nil
+        viewSpy = nil
+        interactorSpy = nil
+        coordinatorSpy = nil
+        super.tearDown()
+    }
+    
+    func test_searchByQuery_CallsInteractorSearchByQuery() {
+        // Given
+        let query = "Test Query"
+        
+        // When
+        sut.searchByQuery(query: query)
+        
+        // Then
+        XCTAssertEqual(interactorSpy.calledMethods, [.searchByQuery(query: query)])
+    }
+    
+    func test_navigateToDetailsById_CallsCoordinatorNavigateToDetailsById() {
+        // Given
+        let showId = 123
+        
+        // When
+        sut.navigateToDetailsById(id: showId)
+        
+        // Then
+        XCTAssertEqual(coordinatorSpy.calledMethods, [.navigateToDetailsById])
+    }
+    
+    func test_searchByQueryWithSuccess_CallsViewShowSearchResults() {
+        // Given
+        let shows = [ShowResponse.dummy()]
+        
+        // When
+        sut.searchByQueryWithSuccess(shows: shows)
+        
+        // Then
+        XCTAssertEqual(viewSpy.calledMethods, [.showSearchResults])
+    }
+    
+    func test_searchByQueryFailure_CallsViewShowError() {
+        // Given
+        let error = NSError(domain: "", code: 0, userInfo: nil)
+        
+        // When
+        sut.searchByQueryFailure(error: error)
+        
+        // Then
+        XCTAssertEqual(viewSpy.calledMethods, [.showError])
+    }
+}
