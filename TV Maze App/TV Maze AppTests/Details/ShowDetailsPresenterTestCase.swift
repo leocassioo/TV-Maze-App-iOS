1b2f250c8cc6335b7730b96bb1aaabd45313427a
Leonardo Figueiredo <leocassio.af@gmail.com>
2024-06-09 14:09:10 -0300

    tests: tests for presenter

---
+++ TV Maze App/TV Maze AppTests/Details/ShowDetailsPresenterTestCase.swift
@@ -0,0 +1,63 @@
+//
+//  ShowDetailsPresenterTestCase.swift
+//  TV Maze AppTests
+//
+//  Created by Leonardo de Cassio Andrade Figueiredo on 09/06/24.
+//
+
+import XCTest
+@testable import TV_Maze_App
+
+final class ShowDetailsPresenterTestCase: XCTestCase {
+    
+    var sut: ShowDetailsPresenter!
+    var viewSpy: ShowDetailsViewControllerSpy!
+    var interactorSpy: ShowDetailsInteractorSpy!
+    
+    override func setUp() {
+        super.setUp()
+        viewSpy = ShowDetailsViewControllerSpy()
+        interactorSpy = ShowDetailsInteractorSpy()
+        sut = ShowDetailsPresenter()
+        sut.view = viewSpy
+        sut.interactor = interactorSpy
+        interactorSpy.output = sut
+    }
+    
+    override func tearDown() {
+        viewSpy = nil
+        interactorSpy = nil
+        sut = nil
+        super.tearDown()
+    }
+    
+    func test_viewDidLoad_CallsFetchShowDetailsAndFetchAliases() {
+        // When
+        sut.viewDidLoad()
+        
+        // Then
+        XCTAssertEqual(interactorSpy.calledMethods, [.fetchShowDetails, .fetchAliases])
+    }
+    
+    func test_didFetchShowDetails_CallsDisplayShowDetails() {
+        // Given
+        let show = Show.dummy()
+        
+        // When
+        sut.didFetchShowDetails(showDetails: show)
+        
+        // Then
+        XCTAssertEqual(viewSpy.calledMethods, [.displayShowDetails])
+    }
+    
+    func test_didFetchAliases_CallsDisplayAliases() {
+        // Given
+        let aliases = [AliaseModel.dummy()]
+        
+        // When
+        sut.didFetchAliases(aliases: aliases)
+        
+        // Then
+        XCTAssertEqual(viewSpy.calledMethods, [.dispayAliases])
+    }
+}
